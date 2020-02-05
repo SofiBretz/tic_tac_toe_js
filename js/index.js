e0844cfbaa5676d125d6f515ae4e275d57af293d
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2020-02-05 14:56:57 -0300

    added first style to the app, added boardDevelopment and configGame to the js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,32 @@
-const gameBoard() => {
-    const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X", ]
+const gameBoard () => {
+    const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X",]
 
 }();
 
-const player = (name, symbol) => {
+let boardDev = () => {
+    const boxSize = 150 + "px"; // cambiar nombre!
+        const container = document.getElementById("boardContainer");
+
+        container.style.gridTemplateColumns = `repeat(3, 1fr)`;
+        container.style.gridTemplateRows = `repeat(3, 1fr)`;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                let div = document.createElement("div");
+                div.style.width = boxSize;
+                div.style.height = boxSize;
+                div.classList.add("boxes"); // cambiar nombre!
+                div.setAttribute("id", "div" + i + j);
+                container.appendChild(div);
+            }
+        };
+}
+
+
+const player = (name) => {
     const array = [];
     let win = false;
+    let score= 0;
     let checkWinner() => {
         const winingCompositions = [
             [1, 2, 3],
@@ -24,21 +45,37 @@ const player = (name, symbol) => {
             }
         }
     }();
+
     return {
         name,
-        symbol,
-        win
+        win,
+        score
     }
 }
 
-let player 1 = new player(name, symbol)
+let configGame = () => {
+    let name1= getElementById("");
+    let name2= getElementById("");
+    let player1 = new player(name);
+    let player2 = new player(name);
 
-let moves = 0
-if (player1.win) {
-    //player 1 wins 
-} else {
-    if moves == 9
-    //draw
+    let moves = 0
+    return {player1, player2, moves}
 }
-moves++
+
+let gameStart = () => {
+
+
+
 }
+
+
+
+/* if (player1.win) {
+        //player 1 wins 
+    } else {
+        if moves == 9
+        //draw
+    }
+    moves++
+    }*/
