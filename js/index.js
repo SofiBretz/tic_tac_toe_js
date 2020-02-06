de23dd01b93e29c44ecea5cf92b1a8d440619807
andynarf <untalandy@gmail.com>
2020-02-06 20:00:54 -0300

    got everything toghether, got ir working, small details yet to be fixed

--- js/index.js
+++ js/index.js
@@ -6,8 +6,21 @@
 
 const Player = (name, symbol) => {
   const array = [];
-  let win = false;
+  const win = false;
   const score = 0;
+
+  return {
+    name,
+    array,
+    symbol,
+    win,
+    score,
+  };
+};
+
+// document.getElementById('startGame').addEventListener('click', configGame)
+
+const gameFlow = () => {
   const checkWinner = () => {
     const winingCompositions = [
       [1, 2, 3],
@@ -17,60 +30,57 @@ const Player = (name, symbol) => {
       [2, 5, 8],
       [3, 6, 9],
       [1, 5, 9],
-      [3, 5, 7]
+      [3, 5, 7],
     ];
-    console.log(win);
     // for (,let i = 0 , i<9, i++)
     for (const x in winingCompositions) {
       // console.log((winingCompositions[x])[0])
       if (
-        array.includes(winingCompositions[x][0]) &&
-        array.includes(winingCompositions[x][1]) &&
-        array.includes(winingCompositions[x][2])
+        currentPlayer.array.includes(winingCompositions[x][0])
+        && currentPlayer.array.includes(winingCompositions[x][1])
+        && currentPlayer.array.includes(winingCompositions[x][2])
       ) {
-        win = true;
-        console.log(win);
-        break;
+        currentPlayer.win = true;
       }
     }
   };
 
-  return {
-    name,
-    array,
-    symbol,
-    win,
-    score,
-    checkWinner
-  };
-};
+  let moves = 0;
 
-// document.getElementById('startGame').addEventListener('click', configGame)
-
-let gameFlow = () => {
-  console.log("funciona");
-
-  const getMark = event => {
+  const getMark = (event) => {
     currentPlayer.array.push(parseInt(event.target.id));
     document.getElementById(event.target.id).innerHTML = currentPlayer.symbol;
+    document.getElementById(event.target.id).removeEventListener('click', getMark);
+    checkWinner();
+    if (currentPlayer.win) {
+      console.log(`${currentPlayer.name} wins`);
+      // sleep(3000);
+      document.location.reload();
+      
+    }
+
+
+    playerSwitch();
+    if (moves > 8) {
+      console.log('DRAW GAME');
+      reset();
+    }
   };
 
-  for (const x of document.getElementsByClassName("cell")) {
-    x.addEventListener("click", getMark);
+  for (const x of document.getElementsByClassName('cell')) {
+    x.addEventListener('click', getMark);
   }
 
   // let configGame = () => {
-  let draw = false;
-  let name1 = document.getElementById("player1Name").value;
-  let name2 = document.getElementById("player2Name").value;
-  let player1 = Player(name1, "X");
-  let player2 = Player(name2, "O");
-  let moves = 0;
-  //return {player1, player2, moves}
-  //}
+  const draw = false;
+  const name1 = document.getElementById('player1Name').value;
+  const name2 = document.getElementById('player2Name').value;
+  const player1 = Player(name1, 'X');
+  const player2 = Player(name2, 'O');
+  // return {player1, player2, moves}
+  // }
 
   let currentPlayer = player1;
-  console.log(currentPlayer.name);
   let playerSwitch = () => {
     if (currentPlayer == player1) {
       currentPlayer = player2;
@@ -79,36 +89,52 @@ let gameFlow = () => {
     }
     moves++;
   };
-
-  let drawCheck = () => {
-    if (moves == 9) {
-      draw == true;
+  let sleep = (milliseconds) => {
+    const start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds) {
+        break;
+      }
     }
   };
 
-  //let playGame= () => {
+  let reset = () => {
+    sleep(2000);
+    document.getElementById(1).innerHTML = ('');
+    document.getElementById(2).innerHTML = ('');
+    document.getElementById(3).innerHTML = ('');
+    document.getElementById(4).innerHTML = ('');
+    document.getElementById(5).innerHTML = ('');
+    document.getElementById(6).innerHTML = ('');
+    document.getElementById(7).innerHTML = ('');
+    document.getElementById(8).innerHTML = ('');
+    document.getElementById(9).innerHTML = ('');
+  };
 
-  //configGame
-  //ciclo
 
-  //currentPlayer.getMark;
-  currentPlayer.checkWinner;
-  /*if (currentPlayer.win) {
+  // let drawCheck = () => {
+
+  // };
+
+  // let playGame= () => {
+
+  // configGame
+  // ciclo
+
+  // currentPlayer.getMark;
+  /* if (currentPlayer.win) {
     //break;
   }
-  
+
   drawCheck;
   if (draw) {
     //break;
-  }*/
-  playerSwitch;
-  console.log(currentPlayer.name);
-  console.log(player1.array);
+  } */
 };
 
-//document.getElementById('startGame').addEventListener('click', gameFlow);
+// document.getElementById('startGame').addEventListener('click', gameFlow);
 
-//}
+// }
 
 // const player1 = Player('test', 'x');
 // const getMark = (event) => {
